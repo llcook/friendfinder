b6181c9f86930bb64f389210a783f4e06a10be80
watercolorhorse <29869241+llcook@users.noreply.github.com>
2018-12-19 22:16:19 -0700

    Created directories and files

---
+++ server.js
@@ -0,0 +1,9 @@
+// Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
+
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express app
+var app = express();
+var PORT = 3000;
